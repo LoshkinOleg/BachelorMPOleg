b0bef4882e224ebe8616607629fdce3e67f2cae6
OlegLoshkin <loshkin.oleg.95@gmail.com>
2022-06-06 14:46:53 +0200

    Made simple sdl app with sound rotating around listener

--- ProfilingApp/src/main.cpp
+++ ProfilingApp/src/main.cpp
@@ -5,50 +5,32 @@
 
 #include <ThreeDTI_AudioRenderer.h>
 
-int TestThreeDTI()
+static void Update(bs::ThreeDTI_AudioRenderer& audioRenderer, bs::SoundMakerId soundId)
 {
-	constexpr const float pi = 3.14f;
-	constexpr const float thirdOfTwoPi = (2.0f * pi) / 3.0f;
-	const float firstPos[3] = {std::sinf(thirdOfTwoPi), std::cosf(thirdOfTwoPi) , 0.0f};
-	const float secondPos[3] = {std::sinf(thirdOfTwoPi * 2.0f), std::cosf(thirdOfTwoPi * 2.0f) , 0.0f};
-	const float thirdPos[3] = {std::sinf(thirdOfTwoPi * 3.0f), std::cosf(thirdOfTwoPi * 3.0f) , 0.0f};
+	static float theta = 0.0f;
 
-	bs::ThreeDTI_AudioRenderer e;
-	e.Init("../resources/HRTF/SOFA/hrtf.sofa", "../resources/BRIR/SOFA/brir.sofa", 2048);
-	auto sound = e.CreateSoundMaker("../resources/AudioSamples/brownNoise_44100Hz_32f_5sec.wav", bs::ClipWrapMode::LOOP);
-	if (sound != bs::INVALID_ID)
-	{
-		float theta = 0.0f;
-		while (true)
-		{
-			e.MoveSoundMaker(sound, std::sinf(theta), std::cosf(theta), 0.0f);
-			theta += 0.05f;
-			Pa_Sleep(40);
-		}
-
-		/*e.MoveSoundMaker(sound, firstPos[0], firstPos[1], firstPos[2]);
-		std::cin.ignore();
-
-		e.ResetSoundMaker(sound);
-		e.MoveSoundMaker(sound, secondPos[0], secondPos[1], secondPos[2]);
-		std::cin.ignore();
-
-		e.ResetSoundMaker(sound);
-		e.MoveSoundMaker(sound, thirdPos[0], thirdPos[1], thirdPos[2]);
-		std::cin.ignore();*/
-	}
-	e.Shutdown();
-
-	return (sound == bs::INVALID_ID);
+	audioRenderer.MoveSoundMaker(soundId, std::sinf(theta), std::cosf(theta), 0.0f);
+	theta += 0.05f;
+	Pa_Sleep(40);
 }
 
-int TestKeyboardInputs()
+static int RunProgram()
 {
-	auto result = SDL_Init(SDL_INIT_EVERYTHING);
-	assert(result == 0, "SDL failed to initialize!");
+	{// Scoped sucess check.
+		auto result = SDL_Init(SDL_INIT_EVERYTHING);
+		assert(result == 0, "SDL failed to initialize!");
+	}
 
 	auto* window = SDL_CreateWindow("ProfilingApp", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 720, 720, 0);
 	assert(window, "SDL failed to create a window!");
+
+	bs::ThreeDTI_AudioRenderer audioRenderer;
+	{// Scoped success chack.
+		auto initSuccess = audioRenderer.Init("../resources/HRTF/SOFA/hrtf.sofa", "../resources/BRIR/SOFA/brir.sofa", 2048);
+		assert(initSuccess, "Failed to initialize audio renderer!");
+	}
+	auto sound = audioRenderer.CreateSoundMaker("../resources/AudioSamples/brownNoise_44100Hz_32f_5sec.wav", bs::ClipWrapMode::LOOP);
+	assert(sound != bs::INVALID_ID, "Failed to load wav file!");
 
 	bool shutdown = false;
 	while (!shutdown)
@@ -76,6 +58,8 @@ int TestKeyboardInputs()
 				}break;
 			default:break;}
 		}
+
+		Update(audioRenderer, sound); // Update audio renderer
 	}
 	SDL_Quit();
 
@@ -84,5 +68,5 @@ int TestKeyboardInputs()
 
 int main()
 {
-	return TestKeyboardInputs();
+	return RunProgram();
 }
