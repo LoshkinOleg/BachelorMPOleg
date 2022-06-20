e290c2f00bd1ccfd3b72b3bb892011d83acf4c97
Oleg Loshkin <loshkin.oleg.95@gmail.com>
2022-06-20 14:14:25 +0200

    Removed all renderers except 3dti

---
+++ ExperimentApp/include/RendererManager.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include "portaudio.h"
+
+#include "ThreeDTI_AudioRenderer.h"
+
+namespace bsExp
+{
+	class RendererManager
+	{
+	public:
+		enum class AudioRendererType: size_t
+		{
+			ThreeDTI = 0,
+
+			MAX = ThreeDTI
+		};
+
+		BS_NON_COPYABLE(RendererManager);
+		BS_NON_MOVEABLE(RendererManager);
+
+		RendererManager();
+		~RendererManager();
+
+		void CallImplementationsUpdates();
+
+		void PlaySound(const char* soundName);
+		void PauseSound(const char* soundName);
+		bool IsPaused(const char* soundName);
+		void StopSound(const char* soundName);
+		void StopAll();
+
+		void MoveSound(const char* soundName, const bs::CartesianCoord coord);
+		void MoveAllSounds(const bs::CartesianCoord coord);
+
+		void SetSelectedRenderer(const AudioRendererType type);
+		AudioRendererType GetSelectedRenderer() const;
+
+		constexpr static float const MIN_SOUND_AZIMUTH = bs::ToRadians(-180.0f);
+		constexpr static float const MAX_SOUND_AZIMUTH = bs::ToRadians(180.0f);
+		constexpr static float const MIN_SOUND_DISTANCE = 0.33f;
+		constexpr static float const MAX_SOUND_DISTANCE = 3.0f;
+		constexpr static float const HEAD_ALTITUDE = 1.3f; // Oleg@self: find a way to pass this as argument to program. Move to application maybe since that's where it's used mostly?
+		constexpr static float const MIN_SOUND_ELEVATION = bs::ToRadians(50.0f);
+		constexpr static float const MAX_SOUND_ELEVATION = bs::ToRadians(120.0f);
+		constexpr static char const* const HRTF_PATH = "../resources/HRTF/3DTI_HRTF_IRC1008_128s_44100Hz.sofa";
+		constexpr static char const* const BRIR_PATH = "../resources/BRIR/3DTI_BRIR_medium_44100Hz.sofa";
+		constexpr static char const* const WAV_PATH_SPEECH = "../resources/AudioSamples/AnechoicSpeech44100.wav";
+		constexpr static char const* const WAV_PATH_BROWN_NOISE = "../resources/AudioSamples/brownNoise_44100Hz_32f_5sec.wav";
+		constexpr static char const* const WAV_PATH_SWEEP = "../resources/AudioSamples/sweep20to16000_44100Hz_32f_10sec.wav";
+		constexpr static size_t const BUFFER_SIZE = 1024;
+		constexpr static size_t const SAMPLE_RATE = 44100;
+		constexpr static float const ROOM_SIZE_X = 7.0f;
+		constexpr static float const ROOM_SIZE_Y = 8.0f;
+		constexpr static float const ROOM_SIZE_Z = 3.0f;
+		constexpr static size_t const MIN_RENDERER_INDEX = 0;
+		constexpr static size_t const MAX_RENDERER_INDEX = (size_t)AudioRendererType::MAX;
+
+	private:
+		static int ServiceAudio_
+		(
+			const void* unused, void* outputBuffer,
+			unsigned long framesPerBuffer, const PaStreamCallbackTimeInfo* timeInfo,
+			PaStreamCallbackFlags statusFlags, void* userData
+		);
+
+		AudioRendererType selectedRenderer_ = AudioRendererType::ThreeDTI;
+		bs::ThreeDTI_AudioRenderer threeDTI_renderer_;
+		std::map<std::string, size_t> threeDTI_soundIds_;
+
+		PaStream* pStream_ = nullptr;
+		std::vector<float> renderResult_;
+	};
+}
---
+++ ExperimentApp/src/Application.cpp
@@ -0,0 +1,99 @@
+#include <Application.h>
+
+bsExp::Application::Application(const size_t randSeed):
+	rndEngine_(RandomEngine(randSeed, RendererManager::MIN_SOUND_DISTANCE, RendererManager::MAX_SOUND_DISTANCE, RendererManager::MIN_SOUND_ELEVATION, RendererManager::MAX_SOUND_ELEVATION, RendererManager::MIN_SOUND_AZIMUTH, RendererManager::MAX_SOUND_AZIMUTH, RendererManager::MIN_RENDERER_INDEX, RendererManager::MAX_RENDERER_INDEX))
+{
+	// DEBUG
+	sdlManager_.RegisterCallback(SdlManager::Input::NumZero, [this]() { rendererManager_.StopAll(); });
+	sdlManager_.RegisterCallback(SdlManager::Input::NumOne, [this]() { rendererManager_.PlaySound("noise"); });
+	sdlManager_.RegisterCallback(SdlManager::Input::NumTwo, [this]() { rendererManager_.PlaySound("speech"); });
+	sdlManager_.RegisterCallback(SdlManager::Input::NumThree, [this]() { rendererManager_.PlaySound("sweep"); });
+	sdlManager_.RegisterCallback(SdlManager::Input::NumFour, [this]() { rendererManager_.PauseSound("noise"); });
+	sdlManager_.RegisterCallback(SdlManager::Input::NumFive, [this]() { rendererManager_.PauseSound("speech"); });
+	sdlManager_.RegisterCallback(SdlManager::Input::NumSix, [this]() { rendererManager_.PauseSound("sweep"); });
+	sdlManager_.RegisterCallback(SdlManager::Input::NumPlus, [this]() { SetRandomSourcePos_(); });
+
+	sdlManager_.RegisterCallback(SdlManager::Input::ArrowUp, [this]() { currentSoundPos_.x += 0.1f; });
+	sdlManager_.RegisterCallback(SdlManager::Input::ArrowDown, [this]() { currentSoundPos_.x -= 0.1f; });
+	sdlManager_.RegisterCallback(SdlManager::Input::ArrowLeft, [this]() { currentSoundPos_.y += 0.1f; });
+	sdlManager_.RegisterCallback(SdlManager::Input::ArrowRight, [this]() { currentSoundPos_.y -= 0.1f; });
+	sdlManager_.RegisterCallback(SdlManager::Input::NumMultiply, [this]() { currentSoundPos_.z += 0.1f; });
+	sdlManager_.RegisterCallback(SdlManager::Input::NumDivide, [this]() { currentSoundPos_.z -= 0.1f; });
+	sdlManager_.RegisterCallback(SdlManager::Input::NumMinus, [this]() { logger_.LogNewSoundPos(currentSoundPos_); });
+	sdlManager_.RegisterCallback(SdlManager::Input::Spacebar, [this]() { SelectRandomRenderer_(); });
+
+	SetRandomSourcePos_();
+	SelectRandomRenderer_();
+}
+
+int bsExp::Application::RunProgram()
+{
+	bool shutdown = false;
+	while (!shutdown)
+	{
+		UpdateTransforms_();
+		rendererManager_.MoveAllSounds(currentSoundPos_);
+		rendererManager_.CallImplementationsUpdates();
+		shutdown = sdlManager_.Update();
+	}
+
+	return 0;
+}
+
+void bsExp::Application::SelectRandomRenderer_()
+{
+	const auto selectedRenderer = (RendererManager::AudioRendererType)rndEngine_.GenMiddleware();
+	rendererManager_.SetSelectedRenderer(selectedRenderer);
+	switch (selectedRenderer)
+	{
+		case RendererManager::AudioRendererType::ThreeDTI:
+		{
+			logger_.LogRendererChange("3dti");
+		}
+		break;
+		default:
+		break;
+	}
+}
+
+void bsExp::Application::UpdateTransforms_()
+{
+	// Dummy implementation, move openvr code here to retireve controller positions.
+	leftControllerPos = rndEngine_.GenCartesian(rendererManager_.HEAD_ALTITUDE);
+	rightControllerPos = rndEngine_.GenCartesian(rendererManager_.HEAD_ALTITUDE);
+}
+
+void bsExp::Application::SetRandomSourcePos_()
+{
+	currentSoundPos_ = rndEngine_.GenCartesian(rendererManager_.HEAD_ALTITUDE);
+	logger_.LogNewSoundPos(currentSoundPos_);
+}
+
+void bsExp::Application::OnLeftTrigger_()
+{
+	logger_.LogControllerPosition("Participant controller", leftControllerPos);
+}
+
+void bsExp::Application::OnRightTrigger_()
+{
+	logger_.LogControllerPosition("Scientist controller", rightControllerPos);
+}
+
+void bsExp::Application::OnRightGrip_()
+{
+	if (rendererManager_.IsPaused("noise"))
+	{
+		rendererManager_.PlaySound("noise");
+		logger_.LogNoiseToggle(true);
+	}
+	else
+	{
+		rendererManager_.PauseSound("noise");
+		logger_.LogNoiseToggle(false);
+	}
+}
+
+void bsExp::Application::OnRightPad_()
+{
+	SelectRandomRenderer_();
+}
