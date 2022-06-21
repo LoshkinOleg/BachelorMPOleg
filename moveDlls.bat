302aa1785baddd8e60b8aee9db211b1d63f58023
Oleg Loshkin <loshkin.oleg.95@gmail.com>
2022-06-21 16:33:02 +0200

    Added back fmod. Added imgui files but didn't integrate them properly yet.

--- moveDlls.bat
+++ moveDlls.bat
@@ -2,7 +2,9 @@ xcopy %~dp0\thirdparty\easy_profiler\bin\*.dll %~dp0\build\ExperimentApp\bin\Rel
 xcopy %~dp0\thirdparty\libsofa\bin\*.dll %~dp0\build\ExperimentApp\bin\Release\. /y /i
 xcopy %~dp0\thirdparty\openvr\bin\*.dll %~dp0\build\ExperimentApp\bin\Release\. /y /i
 xcopy %~dp0\thirdparty\sdl\bin\*.dll %~dp0\build\ExperimentApp\bin\Release\. /y /i
+xcopy %~dp0\thirdparty\fmod\bin\*.dll %~dp0\build\ExperimentApp\bin\Release\. /y /i
 xcopy %~dp0\thirdparty\easy_profiler\bin\*.dll %~dp0\build\ExperimentApp\bin\Debug\. /y /i
 xcopy %~dp0\thirdparty\libsofa\bin\*.dll %~dp0\build\ExperimentApp\bin\Debug\. /y /i
 xcopy %~dp0\thirdparty\openvr\bin\*.dll %~dp0\build\ExperimentApp\bin\Debug\. /y /i
 xcopy %~dp0\thirdparty\sdl\bin\*.dll %~dp0\build\ExperimentApp\bin\Debug\. /y /i
+xcopy %~dp0\thirdparty\fmod\bin\*.dll %~dp0\build\ExperimentApp\bin\Debug\. /y /i
