7c63123380fd26f585b132245b170ac3f5c27cc7
Oleg Loshkin <loshkin.oleg.95@gmail.com>
2022-07-05 10:43:42 +0200

    Commented and arranged control python script visualizer. Graph is now always starts with a fixed range. Position of the head has been added.

--- positionsVisualizerControl.py
+++ positionsVisualizerControl.py
@@ -1,50 +1,90 @@
+# Imports
 import sys
 import ast
 import matplotlib.pyplot as plt
+import numpy as np
+import os.path
 
-def lerp(a: float, b: float, t: float) -> float:
-    """Linear interpolate on the scale given by a to b, using t as the point on that scale.
-    Examples
-    --------
-        50 == lerp(0, 100, 0.5)
-        4.2 == lerp(1, 5, 0.8)
-    """
-    return (1 - t) * a + t * b
+# Global constants
+HEAD_ALTITUDE = 1.2
+FRONT_ARROW_LENGTH = 0.3
+CONDUCTOR_POS_LABEL = "Conductor controller"
+PARTICIPANT_POS_LABEL = "Participant controller"
+CONDUCTOR_COLOR = 'b'
+PARTICIPANT_COLOR = 'r'
+SOUND_MARKER = 'x'
+HEAD_POS_MARKER = 'o'
 
-logFile = open(sys.argv[1])
-lines = logFile.readlines()
+# Global variables
+cPos = [] # Positions the conductor has logged.
+pPos = [] # Positions the participant has logged.
+lines = [] # String data from the log file.
 
-positionsScientist = []
-positionsParticipant = []
-
-for line in lines:
-    matchIdx = line.find("Conductor controller")
-    if (matchIdx != -1):
-        matchIdx = line.find("(")
-        posStr = line[matchIdx:len(line) - 1]
-        pos = ast.literal_eval(posStr)
-        positionsScientist.append(pos)
-    
-    matchIdx = line.find("Participant controller")
-    if (matchIdx != -1):
-        matchIdx = line.find("(")
-        posStr = line[matchIdx:len(line) - 1]
-        pos = ast.literal_eval(posStr)
-        positionsParticipant.append(pos)
-
-if (len(positionsScientist) != len(positionsParticipant)):
-    print("Mismatch between number of scientist and participant data: %d, %d" %(len(positionsScientist),len(positionsParticipant)))
+# Ensure there's a filepath passed to the script.
+if len(sys.argv) < 2:
+    print("Please pass the relative path to .log file as first argument to the command line.")
     quit()
 
+# Try to open and read log file, if it doesn't exist or is empty, display message to user.
+if os.path.isfile(sys.argv[1]):
+    logFile = open(sys.argv[1], 'r')
+    lines = logFile.readlines()
+    logFile.close()
+    if len(lines) < 2:
+        print("Couldn't retireve any text data from file!")
+        quit()
+else:
+    print("Invalid path passed as argument!")
+    quit()
+
+# Traverse list of lines and extract conductor and participant positions.
+for line in lines:
+    matchIdx = line.find(CONDUCTOR_POS_LABEL)
+    if (matchIdx != -1):
+        matchIdx = line.find("(") # Find start of position entry.
+        posStr = line[matchIdx:len(line) - 1]
+        pos = ast.literal_eval(posStr) # Interprets string as a tuple.
+        cPos.append(pos)
+    
+    matchIdx = line.find(PARTICIPANT_POS_LABEL)
+    if (matchIdx != -1):
+        matchIdx = line.find("(") # Find start of position entry.
+        posStr = line[matchIdx:len(line) - 1]
+        pos = ast.literal_eval(posStr) # Interprets string as a tuple.
+        pPos.append(pos)
+
+# Display error if there's a mismatch between the number of positions logged by the conductor and participant. Needs one-to-one match to draw euclidean error.
+if (len(cPos) != len(pPos)):
+    print("Mismatch between number of conductor and participant log entries: %d, %d" %(len(cPos),len(pPos)))
+    quit()
+
+# Configuring plot graph
 fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-ax.set_xlabel('x (m)')
-ax.set_ylabel('y (m)')
-ax.set_zlabel('z (m)')
-for i in range(len(positionsScientist)):
-    scientistPos = positionsScientist[i]
-    participantPos = positionsParticipant[i]
-    ax.scatter(scientistPos[0],scientistPos[1],scientistPos[2],marker='o', color=(0.0, 1.0, 0.0))
-    ax.scatter(participantPos[0],participantPos[1],participantPos[2],marker='o', color=(0.0, 0.0, 1.0))
-    ax.plot([scientistPos[0], participantPos[0]], [scientistPos[1], participantPos[1]], [scientistPos[2], participantPos[2]], color='r')
+ax = fig.add_subplot(projection='3d', proj_type='ortho', autoscale_on=False) # orthographic projection for ease of mesuring if needed
+ax.set_xlabel('x [m]')
+ax.set_ylabel('y [m]')
+ax.set_zlabel('z [m]')
+ax.set_xlim(-2.0, 2.0) # fixed plot range for consistency's sake
+ax.set_ylim(-2.0, 2.0)
+ax.set_zlim(0.0, 2.0)
+ax.set_xticks(np.arange(-2.0, 2.0, 0.5)) # Don't want the axis ticks to change depending on zoom level.
+ax.set_yticks(np.arange(-2.0, 2.0, 0.5))
+ax.set_zticks(np.arange(0.0, 2.0, 0.5))
+ax.legend(loc='upper left', bbox_to_anchor=(-0.4, 1.15)) # Add legend at the top left corner of the screen.
+ax.scatter(0.0, 0.0, HEAD_ALTITUDE, color=CONDUCTOR_COLOR, label='Head position', marker=HEAD_POS_MARKER) # mark head position
+ax.plot([0.0, FRONT_ARROW_LENGTH], [0.0, 0.0], [HEAD_ALTITUDE, HEAD_ALTITUDE], color=CONDUCTOR_COLOR) # mark head's forward direction in the +x direction
+
+# Draw the 3d plot with all positions and their euclidean errors.
+for i in range(len(cPos)):
+    cp = cPos[i]
+    pp = pPos[i]
+    
+    # Mark actual and percieved sound positions. Add a label if this is the first iteration of the loop to indicate the meaning of the symbol.
+    ax.scatter(cp[0],cp[1],cp[2],   marker=SOUND_MARKER,                color=CONDUCTOR_COLOR,   label=('Actual position' if i == 0 else None))
+    ax.scatter(pp[0],pp[1],pp[2],   marker=SOUND_MARKER,                color=PARTICIPANT_COLOR, label=('Percieved position' if i == 0 else None))
+    
+    # Link the two positions with a red line that represents the euclidean error. Add a label if this is the first iteration of the loop to indicate the meaning of the symbol.
+    ax.plot([cp[0], pp[0]],     [cp[1], pp[1]],     [cp[2], pp[2]],     color=PARTICIPANT_COLOR, label=('errEuclidean' if i == 0 else None))
+
+# Display the resulting graph.
 plt.show()
